bd4267db988ae36ac03486087d8dab4c37db6224
dvir-yona <dviryon09@gmail.com>
2025-01-10 23:50:00 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -5,7 +5,6 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const lib_mod = b.createModule(.{
-        .name = "ZigPooling",
         .root_source_file = b.path("src/lib.zig"),
         .target = target,
         .optimize = optimize,
