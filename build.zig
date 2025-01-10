fce3dd80aa69b7bc2de83bf1d44a39606e0c08ce
dvir-yona <dviryon09@gmail.com>
2025-01-11 00:01:56 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -4,25 +4,13 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/lib.zig"),
-        .target = target,
-        .optimize = optimize,
+    const dep_opts = .{ .target = target, .optimize = optimize };
+    _ = dep_opts;
+
+    _ = b.addModule("ZigPooling", .{
+        .root_source_file = .{ .src_path = .{
+            .sub_path = "src/lib.zig",
+            .owner = b,
+        } },
     });
-
-    const lib = b.addStaticLibrary(.{
-        .name = "ZigPooling",
-        .root_module = lib_mod,
-    });
-
-    b.installArtifact(lib);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
 }
